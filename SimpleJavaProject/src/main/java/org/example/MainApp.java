6a692898f01832ebc1ab447cbb23ed7a7f233630
Sumit Agrawal <SumitAgrawal.03071989@gmail.com>
2020-09-20 13:02:41 +0530

    commiting stage changes

---
+++ SimpleJavaProject/src/main/java/org/example/MainApp.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.apache.camel.main.Main;
+
+/**
+ * A Camel Application
+ */
+public class MainApp {
+
+    /**
+     * A main() so we can easily run these routing rules in our IDE
+     */
+    public static void main(String... args) throws Exception {
+        while(true){
+            System.out.println("Kannu");
+            Thread.sleep(1000);
+        }
+
+    }
+
+}
+
